8f362ad8ced9c289ad28dc92795fff1f25e066e9
Michael De La Rue <mdlr@paddle.com>
2019-02-13 17:03:37 +0000

    rework scripts so correct parts run outside docker and more tracing

---
+++ build-script/ubuntu_container_prep.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# this prepares the docker container for the various build and test
+# commands.  It expects to be run _inside_ the container (e.g. with
+# docker exec) and in the base directory of the repo.
+
+apt update
+apt install make
+make --trace deb_install PYTHON="$PYTHON"
+python --version; echo ; python2 --version ; python3 --version
