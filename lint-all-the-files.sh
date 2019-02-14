c895cee516c8472e976a5f2f41dd9d16b70b4788
Michael De La Rue <mdlr@paddle.com>
2019-02-14 00:52:19 +0000

    provide more progress info from lint-all-the-files

--- lint-all-the-files.sh
+++ lint-all-the-files.sh
@@ -14,25 +14,35 @@ then
     SHELLCHECK=shellcheck
 fi
 
-find . \( -type d \( -name venv -o -name .git \) -prune -false \) -o -type f | ( 
+find . \( -type d \( -name venv -o -name .git -o -iname '*py*cache*' \) -prune -false \) -o -type f | (
     failcount=0
     # substatus=0
     while read -r file
     do
 	case "$file" in
 	    ./features*/steps/*.py)
+		echo "flake8 checking $file:"
 		# T499 seems to be a mypy failure
 		flake8 --ignore=W503,E402,E501,F811,T484,T499 "$file" --max-line-length=99 --builtins=given,when,then &&
 		    ! grep --with-filename --line-number 'pdb.set_trace\|FIXME' "$file" ;;
 	    *.py)
+		echo "flake8 checking $file:"
 		# probably an effective bug - T484 is needed because otherwise it fails
 		# claiming "Module '__future__' has no attribute 'annotations'"
 		flake8 --ignore=W503,E402,E5b01,T484,T499 "$file" --max-line-length=99 &&
 		    ! grep --with-filename --line-number 'pdb.set_trace\|FIXME' "$file" ;;
 	    *.yaml | *.yml )
+		echo "yamllint checking $file:"
 		yamllint --format parsable "$file" -d "line-length: {max: 70}";;
 	    *.sh)
-		${SHELLCHECK} -f gcc "$file"
+		echo "shellcheck checking $file:"
+		${SHELLCHECK} -f gcc "$file";;
+	    *.enc | *.env | .gitignore)
+		true;; # we explicitly have nothing to verify
+	    '#*' | *~ | *.pyc | *.retry )
+		true;;
+	    *)
+		echo "no rule for $file";;
 	esac
 	newstat=$?
 	if [[ "$newstat" -gt "$status" ]]
