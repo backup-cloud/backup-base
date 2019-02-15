1712dce9faede1a38fa25b4cfb94f9711e28839a
Michael De La Rue <mdlr@paddle.com>
2019-02-15 09:59:30 +0000

    update deploy keys and configuration

--- build-script/before_deploy.sh
+++ build-script/before_deploy.sh
@@ -3,9 +3,9 @@
 # this prepares to deploy - it expects to be run in the base travis VM
 
 # shellcheck disable=SC2154
-openssl aes-256-cbc -K "$encrypted_a45cc096e026_key" -iv "$encrypted_a45cc096e026_iv" -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K "$encrypted_c2402a3ad637_key" -iv "$encrypted_c2402a3ad637_iv" -in deploy_key.enc -out deploy_key -d
 chmod 400 deploy_key
-./push_on_success.sh
+sh -vx ./push_on_success.sh
 rm deploy_key
 python3 setup.py sdist
 git tag "${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}"
