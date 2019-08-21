b50c3da22defef8a20677272abe491a7a1122837
Michael De La Rue <mdlr@paddle.com>
2019-08-21 12:17:15 +0100

    fix to use python 3.7 consistently and correct comments

--- build-script/before_script.sh
+++ build-script/before_script.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# this prepares the docker container for the various build and test
-# commands.  It expects to be run _inside_ the container (e.g. with
-# docker exec).
+# this prepares the general build environment - it expects to be run
+# in the base travis VM
 
-# DISABLE: this is needed for ansible setup but in the case of Travis we currently prepare the account before the build.
-# # shellcheck disable=SC2154
-# openssl aes-256-cbc -K "$encrypted_c2402a3ad637_key" -iv "$encrypted_c2402a3ad637_iv" -in aws_credentials_travis.yml.enc -out aws_credentials_travis.yml -d
 # shellcheck disable=SC2154
 openssl aes-256-cbc -K "$encrypted_c2402a3ad637_key" -iv "$encrypted_c2402a3ad637_iv" -in encrypted_build_files.tjz.enc -out encrypted_build_files.tjz -d
 tar xjvf encrypted_build_files.tjz 
