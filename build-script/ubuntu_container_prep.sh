84690e81954ed5b77a79f92be074970ffecdef4d
Michael De La Rue <mdlr@paddle.com>
2019-08-21 11:35:54 +0100

    try to see if we can use ubuntu:disco

--- build-script/ubuntu_container_prep.sh
+++ build-script/ubuntu_container_prep.sh
@@ -7,8 +7,13 @@
 set -vx
 
 apt-get update
-apt-get install -y software-properties-common make git python3.7 python3-pip shellcheck libgpgme11 python3-gpg shellcheck ruby-full
-add-apt-repository -y ppa:ansible/ansible-2.7
+
+# for ubuntu < disco python3.7 won't work since we need the
+# python3-gpg library which needs to be system installed.
+# Unfortunately that breaks mypy and some other tests.
+apt-get install -y software-properties-common make git python3 python3-pip shellcheck libgpgme11 python3-gpg shellcheck ruby-full ansible
+# for ubuntu < disco we need to use a newer ansible than the default
+# add-apt-repository -y ppa:ansible/ansible-2.7
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y ansible python-pip python-boto3
 make pip_install
